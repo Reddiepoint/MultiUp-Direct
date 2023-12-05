ff5d2d67a5ebe0cd91c3f280035d10b138519628
Reddiepoint <31847741+Reddiepoint@users.noreply.github.com>
2023-12-05 18:23:57 +0800

    Implement new data structures in links module
    This commit introduces new foundational data structures, including MultiUpLink, DirectLink, and MultiUpLinkInformation to the links module.

---
+++ src/modules/links.rs
@@ -0,0 +1,43 @@
+use std::collections::{BTreeSet, HashMap};
+use serde::Deserialize;
+
+
+
+
+/// Represents a MultiUp link.
+/// Contains the original input link, the fixed mirror link and any direct links.
+pub struct MultiUpLink {
+    pub original_link: String,
+    pub mirror_link: String,
+    pub direct_links: Option<BTreeSet<DirectLink>>
+}
+
+
+/// Represents a direct link within a MultiUp link.
+/// Contains the host, URL, validity and whether the link should be displayed in the output.
+pub struct DirectLink {
+    pub host: String,
+    pub url: String,
+    pub validity: String,
+    pub displayed: String
+}
+
+/// Represents information about a MultiUp link from the MultiUp API.
+/// Contains details such as the request status, file name, size, upload and download dates,
+/// number of downloads, description, and hosts.
+///
+/// When the API returns an error, only the error field will be returned. Otherwise, it will return
+/// `"success"`.
+#[derive(Deserialize)]
+pub struct MultiUpLinkInformation {
+    pub error: String,
+    pub file_name: Option<String>,
+    pub size: Option<u64>,
+    pub date_upload: Option<String>,
+    pub time_upload: Option<u64>,
+    pub date_last_download: Option<String>,
+    pub number_downloads: Option<u64>,
+    pub description: Option<String>,
+    pub hosts: Option<HashMap<String, String>>,
+}
+
